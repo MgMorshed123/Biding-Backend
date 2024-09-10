6206fa13d05c7c3e825f0731b098652b19d10a95
mdarf <mdarfinji45@gmail.com>
2024-09-10 13:18:29 +0800

    cloudinary setup done

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 import app from "./app.js";
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  Cloud_name: process.env.Cloud_name,
+  API_Secret: process.env.API_Secret,
+  api_Key: process.env.api_Key,
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is listening on PORT ${process.env.PORT}`);
