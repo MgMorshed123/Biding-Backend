75729862cc550e081dbaab094d51c457e831fe1f
mdarf <mdarfinji45@gmail.com>
2024-09-16 13:20:26 +0800

    remocve auction added

--- Controller/auctionItemController.js
+++ Controller/auctionItemController.js
@@ -142,6 +142,24 @@ export const getMyAuctionItems = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-export const removeFromAuction = catchAsyncErrors(async (req, res, next) => {});
+export const removeFromAuction = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(new ErrorHandler("Invalid Id Format", 400));
+  }
+
+  const auctionItem = await Auction.findById(id);
+
+  if (!auctionItem) {
+    return next(new ErrorHandler("Auction Not Found ", 400));
+  }
+
+  await auctionItem.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Auction Items Deleted Successfuly",
+  });
+});
 
 export const republishItem = catchAsyncErrors(async (req, res, next) => {});
